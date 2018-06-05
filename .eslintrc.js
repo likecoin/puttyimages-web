e84877a2db9af6a5a1c2794ae48e0d5cd5f9e0a6
William Cheung <wlamc@oursky.com>
2018-06-05 20:50:36 +0800

    Add jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,18 @@ module.exports = {
   root: true,
   parser: 'babel-eslint',
   env: {
+    'jest/globals': true,
     browser: true,
     node: true,
   },
   extends: [
     'airbnb-base',
+    'plugin:jest/recommended',
     'prettier',
   ],
   // required to lint *.vue files
   plugins: [
+    'jest',
     'html',
   ],
   // add your custom rules here
