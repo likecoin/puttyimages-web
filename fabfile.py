6df7660473b354c464c799dc16065ab0bfe71614
Rick Mak <rick.mak@gmail.com>
2018-06-08 11:38:41 +0800

    Update the comments in fabfile to tell why user/rolesdefs is unimportant

--- fabfile.py
+++ fabfile.py
@@ -3,9 +3,12 @@ from fabric.decorators import task
 from fabric.operations import local, put, run
 
 env.shell = '/bin/sh -l -c'
+# The env.user and roledefs is only useful when the script is doing a remote
+# execution via ssh. The user will be the user uesed to login to ssh, and the
+# roledes is the mapping on the enviromnet to the remote machine.
 env.user = 'unimportant'
 env.roledefs.update({
-    'pandawork': ['puttyimage.pandawork.com'] # this is also unimportant
+    'pandawork': ['puttyimage.pandawork.com']
 })
 
 def short_sha():
