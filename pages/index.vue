27b8f9578a49dea4214e3faea27778e14a5fb1c8
Ng Wing Tat, David <i@ngwingt.at>
2018-06-08 13:40:04 +0800

    💄 Minor adjustment on home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,27 @@
 <template>
-  <section class="container">
-    <h1 class="title">
-      USERS
-    </h1>
-    <v-card>
-      <v-list two-line>
-        <v-list-tile v-for="user in users" :key="user.id" nuxt :to="{ name: 'id', params: { id: user.id }}">
-          {{ user.name }}
-        </v-list-tile>
-      </v-list>
-    </v-card>
-  </section>
+  <div class="home-page">
+
+    <section class="container">
+      <h1 class="title">
+        USERS
+      </h1>
+      <v-card>
+        <v-list two-line>
+          <v-list-tile v-for="user in users" :key="user.id" nuxt :to="{ name: 'id', params: { id: user.id }}">
+            {{ user.name }}
+          </v-list-tile>
+        </v-list>
+      </v-card>
+    </section>
+
+    <div class="image-upload-btn-wrapper">
+      <v-btn class="btn--likecoin" color="secondary" depressed>
+        <v-icon>add_circle_outline</v-icon>
+        <span>Upload Your Image</span>
+      </v-btn>
+    </div>
+
+  </div>
 </template>
 
 <script>
@@ -23,15 +34,24 @@ export default {
   },
   head() {
     return {
-      title: 'Users',
+      title: 'puttyimages',
     };
   },
 };
 </script>
 
-<style scoped>
-.title
-{
-  margin: 30px 0;
+<style lang="scss" scoped>
+.image-upload-btn-wrapper {
+  position: fixed;
+  right: 48px;
+  bottom: 72px;
+
+  .btn--likecoin {
+    height: 88px;
+
+    :global(.btn__content) {
+      padding: 0 64px;
+    }
+  }
 }
 </style>
