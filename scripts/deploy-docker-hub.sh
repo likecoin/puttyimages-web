0d6381bbb95a57e39c6bde8d59c2a1158b2cff84
William Cheung <wlamc@oursky.com>
2018-06-11 20:06:52 +0800

    ⚡️ Use production build for deploy

--- scripts/deploy-docker-hub.sh
+++ scripts/deploy-docker-hub.sh
@@ -3,7 +3,7 @@
 >&2 echo "Using user $DOCKER_USERNAME on Docker Hub..."
 
 >&2 echo "Building image for $TRAVIS_REPO_SLUG..."
-docker build -t $TRAVIS_REPO_SLUG:$(git rev-parse --short HEAD) .
+docker build -t $TRAVIS_REPO_SLUG:$(git rev-parse --short HEAD) --file Dockerfile.production .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 >&2 echo "Pusing image to $TRAVIS_REPO_SLUG..."
