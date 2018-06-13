0425ba29691925dd6ddab8567acffb497116fbe5
Rick Mak <rick.mak@gmail.com>
2018-06-13 19:13:10 +0800

    🎨Fix the ipfs test case on incorrectly testing the mock

--- __mocks__/ipfs-api.js
+++ __mocks__/ipfs-api.js
@@ -4,21 +4,51 @@ class IPFSApi {
     this.host = host;
     this.port = port;
     this.protocol = protocol;
-    this.mockFiles = {};
+    this.mockFiles = {
+      '/file': {
+        content: 'file content',
+        hash: 'file hash',
+        path: '/file',
+        size: 12,
+      },
+    };
   }
   setMockFiles = (files) => {
     this.mockFiles = files;
-  }
+  };
+  id = jest.fn().mockImplementation(() =>
+    Promise.resolve({
+      id: 'QmSvsumZkbvYqKj8fxfBhVLiPbMHmxwyhHpWRBqwFgA7r7',
+      publicKey: 'secrest',
+      addresses: [
+        '/ip4/127.0.0.1/tcp/4001/ipfs/QmSvsumZkbvYqKj8fxfBhVLiPbMHmxwyhHpWRBqwFgA7r7',
+        '/ip4/172.23.0.3/tcp/4001/ipfs/QmSvsumZkbvYqKj8fxfBhVLiPbMHmxwyhHpWRBqwFgA7r7',
+      ],
+      agentVersion: 'go-ipfs/0.4.15/',
+      protocolVersion: 'ipfs/0.1.0',
+    })
+  );
+  bootstrap = {
+    add: jest.fn().mockImplementation(() => Promise.resolve()),
+  };
   files = {
-    add: jest.fn().mockImplementation(({ path }) => Promise.resolve(this.mockFiles[path])),
-    cat: jest.fn().mockImplementation((path) => Promise.resolve(this.mockFiles[path].content)),
-  }
+    add: jest
+      .fn()
+      .mockImplementation(({ path }) => Promise.resolve(this.mockFiles[path])),
+    cat: jest
+      .fn()
+      .mockImplementation((path) =>
+        Promise.resolve(this.mockFiles[path].content)
+      ),
+  };
   pin = {
     add: jest.fn().mockImplementation(() => Promise.resolve()),
-  }
+  };
   swarm = {
-    localAddrs: jest.fn().mockImplementation(() => Promise.resolve('localAddrs')),
-  }
+    localAddrs: jest
+      .fn()
+      .mockImplementation(() => Promise.resolve('localAddrs')),
+  };
 }
 
 export default (...args) => new IPFSApi(...args);
