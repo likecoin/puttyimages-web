472182d80299f09df153723c247b4dcbc536e445
William Cheung <wlamc@oursky.com>
2018-06-15 16:25:12 +0800

    🐛 Handle API error

--- server/api/index.js
+++ server/api/index.js
@@ -5,7 +5,21 @@ import assets from './assets';
 
 const router = Router();
 
+const errorHandler = (err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500);
+  return res.json({
+    code: err.code || 10000,
+    error: true,
+    message: err.message,
+  });
+};
+
 router.use(users);
 router.use(assets);
+
+router.use(errorHandler);
 
 export default router;
