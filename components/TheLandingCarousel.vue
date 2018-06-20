9cb523d906ce44c799cac7766a7604bf45b34f3f
Ng Wing Tat, David <i@ngwingt.at>
2018-06-20 17:33:05 +0800

    ✨ Add `<ImageDetailsDialog/>`

--- components/TheLandingCarousel.vue
+++ components/TheLandingCarousel.vue
@@ -8,56 +8,103 @@
 
     <transition name="the-landing-carousel__credits--slide-fade">
       <div
-        :key="activeImage.id"
+        :key="activeImage.url"
         class="the-landing-carousel__credits"
       >
         <div class="like-button">
           <span class="like-button">{{ activeImageLikeCount }} Like</span>
         </div>
         <div>
-          <span class="text--size-12">by</span> {{ activeImage.author }}<br>
+          <span class="text--size-12">by</span> {{ activeImage.user.displayName }}<br>
           <a
             class="link--dark text--underline"
             href="#"
+            @click="useImage"
           >use this image</a>
         </div>
       </div>
     </transition>
 
+    <image-details-dialog
+      v-if="detailsImage"
+      :image="detailsImage"
+      :is-open.sync="isOpenDialog"
+    />
+
   </div>
 </template>
 
 <script>
+import ImageDetailsDialog from '@/components/ImageDetailsDialog';
 import Swiper from './TheLandingCarouselSwiper';
 
 export default {
   name: 'the-landing-carousel',
   components: {
+    ImageDetailsDialog,
     Swiper,
   },
   data() {
     return {
       activeImageIndex: 0,
+      detailsImage: null,
       images: [
         {
-          id: 1,
-          author: 'Bejamin Von',
-          likeCount: 3242,
-          src: 'https://picsum.photos/2560/1440?image=961',
+          user: {
+            likecoinId: 'bejaminvon',
+            displayName: 'Bejamin Von',
+          },
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium pretium tempor. Ut eget imperdiet neque. In volutpat ante semper diam molestie, et aliquam er…',
+          height: 1440,
+          license: {
+            name: 'cc0',
+          },
+          like: {
+            count: 3242,
+          },
+          tags: [{ name: 'Mountain' }, { name: 'Landscape' }, { name: 'Cold' }],
+          url: 'https://picsum.photos/2560/1440?image=961',
+          width: 2560,
         },
         {
-          id: 2,
-          author: 'Tobias',
-          likeCount: 1767,
-          src: 'https://picsum.photos/2560/1440?image=985',
+          user: {
+            likecoinId: 'tobias1',
+            displayName: 'Tobias',
+          },
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium pretium tempor. Ut eget imperdiet neque. In volutpat ante semper diam molestie, et aliquam er…',
+          height: 1440,
+          license: {
+            name: 'cc0',
+          },
+          like: {
+            count: 1767,
+          },
+          tags: [{ name: 'Mountain' }, { name: 'Landscape' }, { name: 'Cold' }],
+          url: 'https://picsum.photos/2560/1440?image=985',
+          width: 2560,
         },
         {
-          id: 3,
-          author: 'David Ng',
-          likeCount: 485,
-          src: 'https://picsum.photos/2560/1440?image=1027',
+          user: {
+            likecoinId: 'davidng',
+            displayName: 'David Ng',
+          },
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium pretium tempor. Ut eget imperdiet neque. In volutpat ante semper diam molestie, et aliquam er…',
+          height: 1440,
+          license: {
+            name: 'cc0',
+          },
+          like: {
+            count: 485,
+          },
+          tags: [{ name: 'Girl' }, { name: 'Women' }],
+          url: 'https://picsum.photos/2560/1440?image=1027',
+          width: 2560,
         },
       ],
+      isOpenDialog: false,
     };
   },
   computed: {
@@ -65,7 +112,13 @@ export default {
       return this.images[this.activeImageIndex];
     },
     activeImageLikeCount() {
-      return this.activeImage.likeCount.toLocaleString('en');
+      return this.activeImage.like.count.toLocaleString('en');
+    },
+  },
+  methods: {
+    useImage() {
+      this.detailsImage = this.activeImage;
+      this.isOpenDialog = true;
     },
   },
 };
