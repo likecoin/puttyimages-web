03534e84fc7a2b218295c7a0c47bc8e4ad4ad06c
Ng Wing Tat, David <i@ngwingt.at>
2018-06-29 19:20:30 +0800

    🐛 Fix no active image error in `<TheLandingCarousel/>`

--- components/TheLandingCarousel.vue
+++ components/TheLandingCarousel.vue
@@ -1,31 +1,19 @@
 <template>
   <div class="the-landing-carousel">
 
-    <swiper
+    <carousel-swiper
       :images="images"
       :active-index.sync="activeImageIndex"
     />
 
-    <transition name="the-landing-carousel__credits--slide-fade">
-      <div
-        :key="activeImage.url"
-        class="the-landing-carousel__credits"
-      >
-        <div class="like-button">
-          <span class="like-button">{{ activeImageLikeCount }} Like</span>
-        </div>
-        <div>
-          <span
-            v-if="activeImageUserDisplayName"
-            class="text--size-12"
-          >by</span> {{ activeImageUserDisplayName }}<br v-if="activeImageUserDisplayName">
-          <a
-            class="link--dark text--underline"
-            href="#"
-            @click="useImage"
-          >use this image</a>
-        </div>
-      </div>
+    <transition
+      v-if="activeImage"
+      name="the-landing-carousel__credits--slide-fade"
+    >
+      <carousel-credits
+        :key="activeImageIndex"
+        :image="activeImage"
+      />
     </transition>
 
   </div>
@@ -34,12 +22,14 @@
 <script>
 import { createMixin as createTheImageDetailsDialogMixin } from '~/components/TheImageDetailsDialog';
 
-import Swiper from './TheLandingCarouselSwiper';
+import CarouselCredits from './TheLandingCarouselCredits';
+import CarouselSwiper from './TheLandingCarouselSwiper';
 
 export default {
   name: 'the-landing-carousel',
   components: {
-    Swiper,
+    CarouselCredits,
+    CarouselSwiper,
   },
   mixins: [createTheImageDetailsDialogMixin()],
   props: {
@@ -57,31 +47,11 @@ export default {
     activeImage() {
       return this.images[this.activeImageIndex];
     },
-    activeImageLikeCount() {
-      const { like } = this.activeImage;
-      return (like ? like.count : 0).toLocaleString('en');
-    },
-    activeImageUserDisplayName() {
-      const { user } = this.activeImage;
-      return user ? user.displayName : '';
-    },
-  },
-  methods: {
-    useImage() {
-      this.setImageDetailsDialog({
-        image: this.activeImage,
-        isFetched: true,
-        isOpen: true,
-        isUseImage: true,
-      });
-    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-@import '~assets/css/classes';
-
 .the-landing-carousel {
   position: fixed;
   z-index: 0;
@@ -91,42 +61,19 @@ export default {
   width: 100vw;
   height: 100vh;
 
-  &__credits {
-    position: absolute;
-    z-index: 1;
-    bottom: 0;
-    left: 0;
-
-    display: flex;
-
-    color: white;
-
-    @extend .mb-64--dy, .ml-64--dy;
-
-    &--slide-fade- {
-      &enter-active,
-      &leave-active {
-        transition: (
-          transform 1s cubic-bezier(0.2, 0.2, 0, 1),
-          opacity 1s cubic-bezier(0.2, 0.2, 0, 1)
-        );
-      }
-      &enter,
-      &leave-to {
-        transform: translateX(10px);
-
-        opacity: 0;
-      }
+  &__credits--slide-fade- {
+    &enter-active,
+    &leave-active {
+      transition: (
+        transform 1s cubic-bezier(0.2, 0.2, 0, 1),
+        opacity 1s cubic-bezier(0.2, 0.2, 0, 1)
+      );
     }
+    &enter,
+    &leave-to {
+      transform: translateX(10px);
 
-    > *:first-child {
-      display: flex;
-      align-items: center;
-
-      margin-right: 16px;
-      padding-right: 16px;
-
-      border-right: solid 1px white;
+      opacity: 0;
     }
   }
 }
---
+++ components/TheLandingCarouselCredits.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="the-landing-carousel-credits">
+    <div class="like-button">
+      <span class="like-button">{{ likeCount }} Like</span>
+    </div>
+    <div>
+      <span
+        v-if="userDisplayName"
+        class="text--size-12"
+      >by</span> {{ userDisplayName }}<br v-if="userDisplayName">
+      <a
+        class="link--dark text--underline"
+        href="#"
+        @click="useImage"
+      >use this image</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'the-landing-carousel-credits',
+  props: {
+    image: {
+      type: Object,
+      default: () => ({
+        like: {
+          count: 0,
+        },
+        user: {},
+      }),
+    },
+  },
+  computed: {
+    likeCount() {
+      return this.image.like.count.toLocaleString('en');
+    },
+    userDisplayName() {
+      return this.image.user.displayName;
+    },
+  },
+  methods: {
+    useImage() {
+      this.setImageDetailsDialog({
+        image: this.image,
+        isFetched: true,
+        isOpen: true,
+        isUseImage: true,
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '~assets/css/classes';
+
+.the-landing-carousel-credits {
+  position: absolute;
+  z-index: 1;
+  bottom: 0;
+  left: 0;
+
+  display: flex;
+
+  color: white;
+
+  @extend .mb-64--dy, .ml-64--dy;
+
+  > *:first-child {
+    display: flex;
+    align-items: center;
+
+    margin-right: 16px;
+    padding-right: 16px;
+
+    border-right: solid 1px white;
+  }
+}
+</style>
