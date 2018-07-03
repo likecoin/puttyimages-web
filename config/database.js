89a2dd18b21e10aa7e2d3c199e6379c36c4e5397
Michael Cheung <mc.kingho@gmail.com>
2018-07-03 20:15:39 +0800

    🔧 Add password in db config

--- config/database.js
+++ config/database.js
@@ -3,6 +3,7 @@ module.exports = {
     database: process.env.DB_NAME || 'postgres',
     dialect: 'postgres',
     host: process.env.DB_HOSTNAME || 'db',
+    password: process.env.DB_PASSWORD || null,
     username: process.env.DB_USERNAME || 'postgres',
   },
 };
