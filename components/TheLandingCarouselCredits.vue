3324e89b99e64a9308766a1c7fa8370f26eb0612
Ng Wing Tat, David <i@ngwingt.at>
2018-07-05 18:12:02 +0800

    💄 Update landing carousel credits

--- components/TheLandingCarouselCredits.vue
+++ components/TheLandingCarouselCredits.vue
@@ -1,23 +1,28 @@
 <template>
   <div class="the-landing-carousel-credits">
-    <div class="like-button">
-      <span class="like-button">
-        {{ $t('Landing.Credit.label.likeCount', { amount: likeCount }) }}
-      </span>
-    </div>
     <div>
-      <i18n path="Landing.Credit.label.byAuthor">
-        <span
-          v-if="userDisplayName"
-          class="text--size-12"
-          place="by"
-        >by</span><span place="author"> {{ userDisplayName }}</span>
-      </i18n><br v-if="userDisplayName">
-      <a
-        class="link--dark text--underline"
-        href="#"
-        @click="$emit('open-details', image, { isUseImage: true })"
-      >{{ $t('Landing.Credit.button.useThisImage') }}</a>
+
+      <div class="like-button">
+        <span class="like-button">
+          {{ $t('Landing.Credit.label.likeCount', { amount: likeCount }) }}
+        </span>
+      </div>
+
+      <div>
+        <i18n path="Landing.Credit.label.byAuthor">
+          <span
+            v-if="userDisplayName"
+            class="text--size-12"
+            place="by"
+          >by</span><span place="author"> {{ userDisplayName }}</span>
+        </i18n><br v-if="userDisplayName">
+        <a
+          class="link--dark text--underline"
+          href="#"
+          @click="$emit('open-details', image, { isUseImage: true })"
+        >{{ $t('Landing.Credit.button.useThisImage') }}</a>
+      </div>
+
     </div>
   </div>
 </template>
@@ -58,39 +63,70 @@ $the-landing-carousel-credits__shadow-color: rgba(0, 0, 0, 0.7);
   bottom: 0;
   left: 0;
 
-  display: flex;
-
   color: white;
 
-  @extend .mb-64--dy, .ml-64--dy;
+  @extend .pb-64--dy, .px-64--dy;
+
+  // The underlay gradient
+  &::before {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+
+    width: 100vw;
+    height: 20vh;
+    min-height: 160px;
+
+    content: '';
+
+    transition: opacity 0.5s ease;
+
+    opacity: 0.3;
+    background-image: linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 0),
+      rgba(0, 0, 0, 0.5)
+    );
+  }
+  &:hover::before {
+    opacity: 0.6;
+  }
 
   &,
   * {
-    text-shadow: 0 0 10px $the-landing-carousel-credits__shadow-color;
+    text-shadow: 0 0 3px $the-landing-carousel-credits__shadow-color;
   }
 
-  > *:first-child {
+  > div {
     position: relative;
 
     display: flex;
-    align-items: center;
 
-    margin-right: 16px;
-    padding-right: 16px;
+    > * {
+      &:first-child {
+        position: relative;
 
-    // Seperator
-    &::after {
-      position: absolute;
-      top: 0;
-      right: 0;
-      bottom: 0;
+        display: flex;
+        align-items: center;
 
-      width: 1px;
+        margin-right: 16px;
+        padding-right: 16px;
 
-      content: '';
+        // Seperator
+        &::after {
+          position: absolute;
+          top: 0;
+          right: 0;
+          bottom: 0;
 
-      background-color: white;
-      box-shadow: 0 0 10px 0 $the-landing-carousel-credits__shadow-color;
+          width: 1px;
+
+          content: '';
+
+          background-color: white;
+          box-shadow: 0 0 3px 0 $the-landing-carousel-credits__shadow-color;
+        }
+      }
     }
   }
 }
